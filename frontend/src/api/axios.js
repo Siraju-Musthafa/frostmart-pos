ca7c18bda060f4e0b398cec93c51157dc1519a52
sirajul musthafa <sirajulmusthafa786@gmail.com>
2026-06-06 10:58:58 +0530

    update api in frontend

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,8 +1,8 @@
 import axios from "axios"
 
 const api = axios.create({
-  baseURL: "https://snowfreez-backend.onrender.com/api",
-})
+  baseURL: import.meta.env.VITE_API_URL,
+});
 
 // ADD TOKEN AUTOMATICALLY
 api.interceptors.request.use((config) => {
